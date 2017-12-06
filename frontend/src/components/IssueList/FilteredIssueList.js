98d5ad31dc025fa93fff2fff861fb280519cd9b9
nikrb <nik_rb@yahoo.com>
2017-12-06 21:31:10 +0000

    refactor: issue list

--- frontend/src/components/IssueList/FilteredIssueList.js
+++ frontend/src/components/IssueList/FilteredIssueList.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import IssueList from './IssueList';
-import {css} from 'aphrodite';
-import S from './styles';
 
 export default (props) => {
   const {data, filter, title} = props;
-  const filtered_list = data
-                        .filter( item => item.status === filter)
-                        .map( (item, i) => (
-                          <li className={css(S.issue_style)} key={i}>
-                            {item.title}
-                          </li>
-                        ));
+  const filtered_list = data.filter( item => item.status === filter);
   return (
     <IssueList items={filtered_list} title={title} />
   );
--- frontend/src/components/IssueList/IssueList.js
+++ frontend/src/components/IssueList/IssueList.js
@@ -2,13 +2,20 @@ import React from 'react';
 import {css} from 'aphrodite';
 import S from './styles';
 
-export default (props) => (
-  <div className={css(S.column_wrapper)}>
-    <div className={css(S.title)}>
-      {props.title}
+export default (props) => {
+  const items = props.items .map( (item, i) => (
+    <li className={css(S.issue_style)} key={i}>
+      {item.title}
+    </li>
+  ));
+  return (
+    <div className={css(S.column_wrapper)}>
+      <div className={css(S.title)}>
+        {props.title}
+      </div>
+      <ul>
+        {items}
+      </ul>
     </div>
-    <ul>
-      {props.items}
-    </ul>
-  </div>
-);
+  );
+};
