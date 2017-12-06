1532403ad70efe0fd942b0a60b1c002e314fc6ad
nikrb <nik_rb@yahoo.com>
2017-12-06 19:31:27 +0000

    remove unused def

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Routes from './routes/Routes';
-import { Link } from 'react-router-dom';
 import NavBar from './components/NavBar';
 
 export default class App extends Component {
