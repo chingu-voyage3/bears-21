76f7c15862806e7f464b885946e8a9b163c7bb6c
nikrb <nik_rb@yahoo.com>
2017-12-06 20:16:09 +0000

    add dashboard routing

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import NotFound from '../components/NotFound';
 import Dummy from '../components/Dummy';
+import Dashboard from '../containers/Dashboard';
 
 const auth = false;
 
 export default () => {
-	return (	
+	return (
 		<Switch>
 
 			<Route exact path="/" component={Dummy} />
 			<Route path="/login" component={Dummy} />
 			<Route path="/register" component={Dummy} />
 			<Route path="/forgot" component={Dummy} />
+			<Route path="/dashboard" component={Dashboard} />
 			<AuthRoute path="/admin" component={Dummy} pathname={ "/login" } />
-			<AuthRoute path="/dashboard" component={Dummy} pathname={ "/login" } />
 			<AuthRoute path="/newissue" component={Dummy} pathname={ "/login" } />
 			<Route path="*" component={NotFound} />
 
 		</Switch>
 	);
+	// <AuthRoute path="/dashboard" component={Dashboard} pathname={ "/login" } />
 };
 
 
 const AuthRoute = ({ component: Component, pathname: path, ...rest }) => (
 	<Route {...rest} render={ props => (
-		auth ? ( 
-			<Component {...props} /> 
-		) : ( 
-			<Redirect 
-				to={{ pathname: path }} 
+		auth ? (
+			<Component {...props} />
+		) : (
+			<Redirect
+				to={{ pathname: path }}
 			/>
 
 
 		))} />
-	
+
 
 
 
 );
-
