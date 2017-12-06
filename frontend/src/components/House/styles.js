70b6d66de74786fbcea0795681922205ea5c7ebb
nikrb <nik_rb@yahoo.com>
2017-12-06 20:58:39 +0000

    fix aphrodite style creation

--- frontend/src/components/House/styles.js
+++ frontend/src/components/House/styles.js
@@ -1,18 +1,18 @@
 import {StyleSheet} from 'aphrodite';
 
-const styles = {
+const styles = StyleSheet.create({
   list: {
     display: "flex",
     flexDirection: "column"
-  }
+  },
   wrapper: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-around"
-  }
+  },
   house_image: {
     width: "200px"
   }
-};
+});
 
 export default styles;
