629972954e4a917dd77c727d18964bedc720cf23
nikrb <nik_rb@yahoo.com>
2017-12-06 20:46:43 +0000

    use aphrodite for styling

---
+++ frontend/src/components/House/styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'aphrodite';
+
+const styles = {
+  list: {
+    display: "flex",
+    flexDirection: "column"
+  }
+  wrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  }
+  house_image: {
+    width: "200px"
+  }
+};
+
+export default styles;
---
+++ frontend/src/components/IssueList/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'aphrodite';
+
+const styles = StyleSheet.create({
+  column_wrapper: {
+    padding: "1em 0px",
+    width: "30%",
+    display: "flex",
+    flexDirection: "column",
+    border: "1px solid lightgrey"
+  },
+  issue_style: {
+    cursor: "pointer",
+    textAlign: "left",
+    listStyle: "none",
+    ':hover': {
+      background: "lightgrey"
+    }
+  }
+});
+
+export default styles;
