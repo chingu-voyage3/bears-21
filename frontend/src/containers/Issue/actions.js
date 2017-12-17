95d58b42a96633c31bfb84b8237850c2f48c2985
nikrb <nik_rb@yahoo.com>
2017-12-17 22:00:24 +0000

    create stub for fetching issues

--- frontend/src/containers/Issue/actions.js
+++ frontend/src/containers/Issue/actions.js
@@ -1,3 +1,5 @@
+import {checkStatus, parseJSON} from '../../util';
+
 export const getIssue = () => {
   // eslint-disable-next-line no-undef, no-unused-vars
   return new Promise( (resolve, reject) => {
@@ -12,4 +14,13 @@ export const getIssue = () => {
       ]
     });
   });
+};
+
+export const uploadImage = ( payload) => {
+  return fetch( "/api/upload", {
+    method: "post",
+    body: payload
+  })
+  .then( checkStatus)
+  .then( parseJSON);
 };
