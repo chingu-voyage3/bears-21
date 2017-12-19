8a4c2a016f8efc3e52832ad10294581c23bf19bf
Vivek Poddar <vivekimsit@gmail.com>
2017-12-19 15:10:29 +0000

    Setup test framework

--- server/models/user/user.js
+++ server/models/user/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 mongoose.Promise = global.Promise;
-const md5 = require('md5');
 const validator = require('validator');
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
 const passportLocalMongoose = require('passport-local-mongoose');
