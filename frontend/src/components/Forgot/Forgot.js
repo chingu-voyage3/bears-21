ee4ff35d5662fb0adb97cce3a87e353d3a1fad59
mortuie <jimmybob-leon@hotmail.co.uk>
2017-12-19 21:13:40 +0000

    Adding to the forgot password page

--- frontend/src/components/Forgot/Forgot.js
+++ frontend/src/components/Forgot/Forgot.js
@@ -9,9 +9,12 @@ export default class Forgot extends Component {
 
     return (
     
-      <div>
-        HELLO
+      <div className={css(styles.centre)}>
+        <div className={css(styles.mainBox, styles.centre)}>
+          <div>Forgot Password</div>
+          <textarea className={css(styles.textarea)}></textarea>
 
+        </div>
 
       </div>
     
@@ -24,7 +27,21 @@ export default class Forgot extends Component {
 }
 
 const styles = StyleSheet.create({
-  
+  centre: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContext: "center",
+    alignItems: "center",
+  },
+  mainBox: {
+    border: "1px black solid",
+    marginTop: 150,
+    height: 200,
+    width: 450,
+  },
+  textarea: {
+    resize: "none",
+  },
   
   
 });
