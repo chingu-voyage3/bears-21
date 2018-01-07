9b8c9c1c868be89a4b261066cf2507ed9a2801f6
nikrb <nik_rb@yahoo.com>
2018-01-07 14:47:34 +0000

    add new issue _id to house model issues list

--- web/routes/issues/upsert.js
+++ web/routes/issues/upsert.js
@@ -1,9 +1,17 @@
 'use strict'
 const Issue = require('../../../models/issue');
+const House = require('../../../models/house');
 
 async function upsert( req, res) {
+  const new_issue = req.body._id?false:true;
   const issue = new Issue( req.body);
-  await issue.save();
+  issue.isNew = new_issue;
+  issue.save();
+  if( new_issue) {
+    const house = await House.findById( req.body.house);
+    house.issues.push( issue._id);
+    house.save();
+  }
   res.json( {issue});
 }
 
