c6fd5a9ecfbcbfbce82f666da5941259505540ba
nikrb <nik_rb@yahoo.com>
2018-01-07 08:26:59 +0000

    update IssueList to handle issue selection

--- frontend/src/components/IssueList/IssueList.js
+++ frontend/src/components/IssueList/IssueList.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Issue from './Issue';
 import {css} from 'aphrodite';
 import styles from './styles';
 
-export default (props) => {
-  const items = props.items.map( (item, i) => (
-    <li className={css(styles.issue_style)} key={i}>
-      {item.title}
-    </li>
-  ));
-  return (
-    <div className={css(styles.column_wrapper)}>
-      <div className={css(styles.title)}>
-        {props.title}
+export default class IssueList extends Component {
+  // TODO: should we need this?
+  onIssueClick = (issue) => {
+    this.props.onIssueClick( issue);
+  };
+  render = () => {
+    console.log( "IssueList props:", this.props);
+    const items = this.props.items.map( (item, i) => (
+      <Issue issue={item} key={i} onIssueClick={this.onIssueClick} />
+    ));
+    return (
+      <div className={css(styles.column_wrapper)}>
+        <div className={css(styles.title)}>
+          {this.props.title}
+        </div>
+        <ul>
+          {items}
+        </ul>
       </div>
-      <ul>
-        {items}
-      </ul>
-    </div>
-  );
+    );
+  };
 };
