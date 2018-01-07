7b1906b1ac28201cc7a9efc9b80cd8a895ea61e1
nikrb <nik_rb@yahoo.com>
2018-01-07 10:44:21 +0000

    add new issue button and redirect

--- frontend/src/components/House/House.js
+++ frontend/src/components/House/House.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {css} from 'aphrodite';
 import styles from './styles';
 
-export default (props) => {
-  return (
-    <div>
-      <img src={props.src} alt="noimg" />
-      <div className={css(styles.title)}>
-        House
+export default class House extends Component {
+  onNewIssue = () => {
+    this.props.onNewIssue( this.props.data);
+  };
+  render = () => {
+    return (
+      <div>
+        <img src={this.props.data.house_image} alt="noimg" />
+        <div className={css(styles.title)}>
+          House
+          <button type="button" onClick={this.onNewIssue} >+</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
