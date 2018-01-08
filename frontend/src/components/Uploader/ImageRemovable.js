2f6d7966c6aebd57bf4aabc0319aa718f2129c2e
nikrb <nik_rb@yahoo.com>
2018-01-08 19:10:04 +0000

    create an image component with a cross box

---
+++ frontend/src/components/Uploader/ImageRemovable.js
@@ -0,0 +1,29 @@
+import React from 'react';
+export class ImageRemovable extends React.Component {
+  onRemove = () => {
+    this.props.onRemove( this.props.ndx);
+  };
+  render = () => {
+    const {src} = this.props;
+    const img_style = {
+      maxWidth: "200px",
+      maxHeight: "200px"
+    };
+    const close_button = {
+      position: "absolute",
+      top: "0px",
+      right: "0px"
+    };
+    const wrapper = {
+      position: "relative"
+    };
+    return (
+      <div style={wrapper}>
+        <div style={close_button}>
+          <button type="button" onClick={this.onRemove} >X</button>
+        </div>
+        <img src={src} style={img_style} alt="" />;
+      </div>
+    );
+  };
+};
