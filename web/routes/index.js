3ec2caa39f6a74ff4c81fd5799d491353e5c1836
nikrb <nik_rb@yahoo.com>
2018-01-09 08:28:47 +0000

    add image upload api endpoint

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,11 +1,15 @@
 'use strict'
 
 const { Router } = require('express');
+const multer = require('multer');
+const upload = multer({ dest: process.env.IMAGE_UPLOAD_DIR});
+
 const auth = require('./auth');
 const houses = require('./houses');
 const users = require('./users');
 const issues = require('./issues');
 const { catchAsyncErrors } = require('../utils');
+const images = require('./images');
 
 const router = new Router();
 
@@ -15,6 +19,8 @@ router.post('/api/v1/houses', auth.isLoggedIn, catchAsyncErrors(houses.create));
 router.get( '/api/v1/house-issues', auth.isLoggedIn, houses.houseIssueList);
 
 router.post('/api/v1/issues', auth.isLoggedIn, catchAsyncErrors( issues.upsert));
+
+router.post('/api/v1/images', upload.array( 'pic', 3), images.upload);
 
 /**
  * 1. Validate the registration data
