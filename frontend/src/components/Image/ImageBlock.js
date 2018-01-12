45ea2f53c60581a1cc814e16206659bf021823c5
nikrb <nik_rb@yahoo.com>
2018-01-12 12:49:47 +0000

    fix: missing semi-colons

--- frontend/src/components/Image/ImageBlock.js
+++ frontend/src/components/Image/ImageBlock.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import {Uploader} from '../Uploader';
 import {css} from 'aphrodite';
 import styles from './styles';
--- frontend/src/components/Image/ImageList.js
+++ frontend/src/components/Image/ImageList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import ImageRemovable from './ImageRemovable';
 import {css} from 'aphrodite';
 import styles from './styles';
