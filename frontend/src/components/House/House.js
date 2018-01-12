8ed1ee992d654b3529b9e76ffdb42190c39a815c
nikrb <nik_rb@yahoo.com>
2018-01-12 22:58:37 +0000

    review: remove redundant function brackets and refactor css

--- frontend/src/components/House/House.js
+++ frontend/src/components/House/House.js
@@ -4,24 +4,19 @@ import {css} from 'aphrodite';
 import styles from './styles';
 
 export default class House extends Component {
-  onNewIssue = (e) => {
+  onNewIssue = e => {
     console.log( "new issue");
     e.stopPropagation();
-    this.props.onNewIssue( this.props.data);
+    this.props.onNewIssue(this.props.data);
   };
-  onEditHouse = (e) => {
+  onEditHouse = e => {
     console.log( "edit house");
-    this.props.onEditHouse( this.props.data);
+    this.props.onEditHouse(this.props.data);
   };
   render = () => {
     const house_image = this.props.data.images[0];
-    const style = {
-      cursor: "pointer",
-      maxWidth:"215px",
-      textAlign:"center"
-    };
     return (
-      <div style={style} onClick={this.onEditHouse} >
+      <div className={css(styles.house_wrapper)} onClick={this.onEditHouse} >
         <ImageDefault src={house_image} missing_url="//via.placeholder.com/200x200?No Image"/>
         <div className={css(styles.title)}>
           {this.props.data.title}
