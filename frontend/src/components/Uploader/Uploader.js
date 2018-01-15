8cb7feff0adc5b845cace77ffde00280909e3312
nikrb <nik_rb@yahoo.com>
2018-01-15 00:48:45 +0000

    conslidate/tweak Uploader styling

--- frontend/src/components/Uploader/Uploader.js
+++ frontend/src/components/Uploader/Uploader.js
@@ -2,8 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Dropzone from 'react-dropzone';
 import {ImageDefault} from '../Image';
-import {css} from 'aphrodite';
-import styles from './styles';
+import {StyleSheet, css} from 'aphrodite';
 
 export default class Uploader extends Component {
   static propTypes = {
@@ -77,3 +76,33 @@ export default class Uploader extends Component {
     );
   };
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: "0px 20px",
+    border: "2px solid darkgrey",
+    borderRadius: "10px",
+    boxShadow: "0px 0px 8px 0 #ccc",
+    background: "lightgrey",
+    maxWidth: "240px"
+  },
+  well: {
+    margin: "5px auto",
+    // padding: "10px",
+    // paddingBottom: "10px",
+    // marginBottom: "0px"
+  },
+  image_style: { maxWidth: "215px", maxHeight: "215px" },
+  ip_wrapper: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  btn_wrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    fontSize: "0.8em",
+    margin: "1em"
+  }
+});
