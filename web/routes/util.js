b50e8b75f7c76ef90508d9d8c3ec0ee746c7b63b
nikrb <nik_rb@yahoo.com>
2018-01-20 22:34:35 +0000

    tweak debug to update heroku

--- web/routes/util.js
+++ web/routes/util.js
@@ -83,7 +83,7 @@ module.exports = {
     let ret = []; // blobs.map( async (fd) => {
     for( let i=0; i<blobs.length; i++){
       const fd = blobs[i];
-      console.log( "blob file path:", fd.path);
+      console.log( "blob file temp path:", fd.path);
       const data = fs.readFileSync(fd.path);
       const image = new Image( {
         data, contentType: fd.mimetype
