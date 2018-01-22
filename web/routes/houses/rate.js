eaf8ab6d7a1747469e971403d05a0202f3227a3c
nikrb <nik_rb@yahoo.com>
2018-01-22 23:57:51 +0000

    change: console to logger

--- web/routes/houses/rate.js
+++ web/routes/houses/rate.js
@@ -1,8 +1,10 @@
 'use strict'
 const House = require('../../../models/house');
+const logger = require('../../logger');
 
 async function rate( req, res) {
   const {parent_id, value} = req.body;
+  logger.info( `house rate user[${req.user._id}] value[${value}] parent_id[${parent_id}]`);
   const house = await House.findById(parent_id);
   const ratings = house.ratings.filter(
     rating => rating.user.toString() !== req.user._id.toString());
