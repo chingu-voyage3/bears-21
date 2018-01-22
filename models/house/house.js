b5d6283db88a3dcae580cc316f984da185bdc7a2
nikrb <nik_rb@yahoo.com>
2018-01-22 16:01:24 +0000

    add calculated overall rating to house db model

--- models/house/house.js
+++ models/house/house.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const Issue = mongoose.model('Issue').schema;
 const { Schema } = mongoose;
 const slug = require('slug');
 mongoose.Promise = global.Promise;
@@ -59,8 +58,9 @@ const houseSchema = new Schema({
     }],
     default: []
   },
-  active: {type: Boolean, default: true}
-});
+  active: {type: Boolean, default: true},
+},
+{ toObject: { virtuals: true }, toJSON: {virtuals: true} });
 
 houseSchema.pre('save', function(next) {
   if (!this.isModified('title')) {
@@ -70,17 +70,24 @@ houseSchema.pre('save', function(next) {
   next();
 });
 
-houseSchema.virtual('rating').get( function(){
-  const total = this.ratings.reduce( (acc,cur) => {
-    acc[cur.value-1] += 1;
-    return acc;
-  }, [0,0,0,0,0])
-  .reduce( (acc, cur, ndx) => {
-    return acc + cur * (ndx+1);
-  }, 0);
-
-  return total/this.ratings.length;
+houseSchema.virtual( 'rating')
+.get( function(){
+  return calculateRating( this.ratings);
 });
+
+const calculateRating = (ratings) => {
+    if( ratings.length === 0) return 0;
+
+    const total = ratings.reduce( (acc,cur) => {
+      acc[cur.value-1] += 1;
+      return acc;
+    }, [0,0,0,0,0])
+    .reduce( (acc, cur, ndx) => {
+      return acc + cur * (ndx+1);
+    }, 0);
+
+    return total/ratings.length;
+};
 
 houseSchema.statics.findWithIssues = function findWithIssues(req, res) {
   return this.find( { owner: req.user._id, active: true})
@@ -91,7 +98,15 @@ houseSchema.statics.findWithIssues = function findWithIssues(req, res) {
       console.error( "house issues findWithIssues failed:", err);
       res.json( []);
     } else {
-      res.json( docs);
+      const ret = docs.map( doc => {
+        const {title, slug, description, created,
+          location, issues, owner, images} = doc;
+        const r = { title, slug, description, created,
+          location, issues, owner, images};
+        r.rating = calculateRating( doc.ratings);
+        return r;
+      });
+      res.json( ret);
     }
   });
 };
