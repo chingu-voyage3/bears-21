5c7c17bf30116a5ebf53b5d1676dabd724b847d1
nikrb <nik_rb@yahoo.com>
2018-01-23 12:06:39 +0000

    refactor star symbol

--- frontend/src/Rating/Star.js
+++ frontend/src/Rating/Star.js
@@ -1,37 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import StarSymbol from './StarSymbol';
 
-export default class Star extends React.Component {
-  static propTypes = {
-    ratingValue: PropTypes.number.isRequired,
-    lit: PropTypes.bool.isRequired,
-    selected: PropTypes.func.isRequired,
-    onEnter: PropTypes.func.isRequired,
+const Star = (props) => {
+  const onEnter = () => {
+    props.onEnter(props.ratingValue);
   };
-  onEnter = () => {
-    this.props.onEnter(this.props.ratingValue);
-  };
-  selected = e => {
+  const selected = e => {
     e.stopPropagation();
-    this.props.selected(this.props.ratingValue);
+    props.selected(props.ratingValue);
   };
-  render = () => {
-    const filled_star = String.fromCharCode(9733);
-    const line_star = String.fromCharCode(9734);
-    const {lit} = this.props;
-    const star = {
-      color: lit?'gold':'black',
-      fontWeight: 'normal'
-    };
-    return (
-      <span style={star}
-        onMouseEnter={this.onEnter}
-        onClick={this.selected}>
-        {lit
-          ? filled_star
-          : line_star
-        }
-      </span>
-    );
+  const {lit} = props;
+  const star = {
+    color: lit?'gold':'black',
+    fontWeight: 'normal'
   };
-}
+  return (
+    <span style={star}
+      onMouseEnter={onEnter}
+      onClick={selected}>
+      <StarSymbol lit={lit} />
+    </span>
+  );
+};
+
+Star.propTypes = {
+  ratingValue: PropTypes.number.isRequired,
+  lit: PropTypes.bool.isRequired,
+  selected: PropTypes.func.isRequired,
+  onEnter: PropTypes.func.isRequired,
+};
+
+export default Star;
