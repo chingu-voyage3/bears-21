9946e71929c9fb54c40c322a85fb4f7f91b66b1d
nikrb <nik_rb@yahoo.com>
2018-01-23 00:19:10 +0000

    fix: lint backend

--- web/routes/images/grab.js
+++ web/routes/images/grab.js
@@ -3,7 +3,6 @@
 const Image = require( '../../../models/image');
 
 async function grab( req, res) {
-  console.log( "image request:", req.params);
   const image = await Image.findById( req.params.id);
   res.setHeader( 'content-type', image.contentType);
   res.send(image.data);
