a717caddc6e8324355ca69d0059d512c5630b322
nikrb <nik_rb@yahoo.com>
2018-01-26 23:46:43 +0000

    fix: check login on start
    on startup check server for user logged in.
    added user name to nav menu bar

--- frontend/src/Entrypoint/Main.js
+++ frontend/src/Entrypoint/Main.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Routes from '../Routes/Routes';
 import { Navbar } from '../Navbar';
 import { Footer } from '../Footer';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { StyleSheet, css } from 'aphrodite';
+import {autoLogin} from '../Redux/actions';
 
 class app extends Component {
+  static propTypes = {
+    autoLogin: PropTypes.func.isRequired
+  };
+  componentWillMount = () => {
+    this.props.autoLogin();
+  };
+  componentWillReceiveProps = newProps => {
+    if( newProps.user) {
+      localStorage.setItem( 'user', JSON.stringify(newProps.user));
+    }
+  };
   render() {
     return (
       <div className={css(styles.container)}>
@@ -29,5 +42,16 @@ const styles = StyleSheet.create({
   }
 });
 
-const App = withRouter(connect()(app));
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    autoLogin: () => dispatch( autoLogin())
+  };
+};
+
+const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
 export default App;
