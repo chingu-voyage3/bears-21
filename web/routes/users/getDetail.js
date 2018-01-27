ad88a0f45e617d2178b1c5a9d8537f1d5b29bccd
nikrb <nik_rb@yahoo.com>
2018-01-27 23:04:48 +0000

    change: return _id with user endpoints

--- web/routes/users/getDetail.js
+++ web/routes/users/getDetail.js
@@ -3,8 +3,11 @@
 const User = require('../../../models/user');
 
 async function getDetail( req, res) {
-  const user = await User.findById( req.user._id);
+  const user_id = req.params.id || req.user._id;
+  const user = await User.findById( user_id);
+  console.log( "user id:", user._id);
   res.json({
+    _id: user._id,
     name: user.name,
     avatar: user.avatar,
     email: user.email
