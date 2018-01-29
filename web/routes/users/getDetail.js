c082a6dad5e6045ffea8102a298641045b1ab240
nikrb <nik_rb@yahoo.com>
2018-01-29 16:50:20 +0000

    fix: linting

--- web/routes/users/getDetail.js
+++ web/routes/users/getDetail.js
@@ -5,7 +5,6 @@ const User = require('../../../models/user');
 async function getDetail( req, res) {
   const user_id = req.params.id || req.user._id;
   const user = await User.findById( user_id);
-  console.log( "user id:", user._id);
   res.json({
     _id: user._id,
     name: user.name,
