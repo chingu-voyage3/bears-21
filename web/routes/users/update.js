1c3efce164b7296c8909ea01553f7365ba5e066a
nikrb <nik_rb@yahoo.com>
2018-01-30 16:23:57 +0000

    fix: backend avatar pic upload

--- web/routes/users/update.js
+++ web/routes/users/update.js
@@ -1,16 +1,24 @@
 'use strict'
-const User = require('../../models/user');
+const User = require('../../../models/user');
+const util = require('../util');
 
 async function update( req, res) {
-  const user = User.findById( req.user._id);
+  const user = await User.findById( req.user._id);
   const {name, email, avatar} = req.body;
   user.name = name;
   user.email = email;
-  if( avatar)
+  if (req.files.length) {
+    const image_ids = await util.saveBlobs( req.files);
+    user.avatar = image_ids[0];
+  } else if( avatar){
+    user.avatar = avatar;
+  }
 
   try {
     await user.save();
+    console.log( "user updated:", user);
   } catch( e) {
+    console.error("user save failed:", e);
     res.json({success: false, message: e});
     return;
   }
