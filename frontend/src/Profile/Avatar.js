4fec4e1a6c3c351233d167fc802f669d37962e61
nikrb <nik_rb@yahoo.com>
2018-01-30 16:32:04 +0000

    simplify profile avatar

--- frontend/src/Profile/Avatar.js
+++ frontend/src/Profile/Avatar.js
@@ -1,44 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {ImageDefault} from '../Image';
 import {Uploader} from '../Uploader';
 import {StyleSheet, css} from 'aphrodite';
-import loadImage from '../Image/actions';
 
-class Avatar extends Component {
-  state = {
-    image_src: null
-  };
-  componentDidMount = () => {
-    const src = this.props.image;
-    // FIXME: this code is duplicated from ImageDefault. We now set the image as a
-    // background in Dropzone which I only just sussed out, which means ImageDefault
-    // isn't used (for Uploader). ImageDefault has an issue with loading images
-    // (cf FIXME in file) so that needs refactoring and the image load done
-    // higher up component chain.
-    if( src.indexOf( '/') === -1){
-      loadImage( src)
-      .then( url => {
-        this.setState( {image_src: url});
-      });
-    } else {
-      this.setState( {image_src: src});
+const Avatar = ({name, localUser, image, changeImage}) => (
+  <div className={css(styles.wrapper)}>
+    {localUser
+      ? <Uploader currentImage={image} addImage={changeImage} />
+      : <ImageDefault src={image} missing_url='//via.placeholder.com/200x200?text=noimg' />
     }
-  };
-  render = () => {
-    const {image_src} = this.state;
-    const {name, localUser, changeImage} = this.props;
-    return (
-      <div className={css(styles.wrapper)}>
-        {localUser
-          ? <Uploader currentImage={image_src} addImage={changeImage} />
-          : <ImageDefault src={image_src} missing_url='//via.placeholder.com/200x200?text=noimg' />
-        }
-        <p>{name}</p>
-      </div>
-    );
-  };
-}
+    <p>{name}</p>
+  </div>
+);
 
 Avatar.propTypes = {
   localUser: PropTypes.bool.isRequired,
