55e0743f08943784293ed4275f4063eebd6c46ef
nikrb <nik_rb@yahoo.com>
2018-02-01 23:35:46 +0000

    refactor image default loading

--- frontend/src/Image/index.js
+++ frontend/src/Image/index.js
@@ -1,7 +1,8 @@
+import ImageRef from './ImageRef';
 import ImageBlock from './ImageBlock';
 import ImageDefault from './ImageDefault';
 import ImageList from './ImageList';
 import ImageListRemovable from './ImageListRemovable';
 import ImageRemovable from './ImageRemovable';
 
-export {ImageBlock, ImageDefault, ImageList, ImageListRemovable, ImageRemovable};
+export {ImageRef, ImageBlock, ImageDefault, ImageList, ImageListRemovable, ImageRemovable};
