13812440a8b92bed98d1b9ee4555cc557a52282f
nikrb <nik_rb@yahoo.com>
2018-02-11 01:29:50 +0000

    fix: remove duplicate search redux actions

--- frontend/src/Redux/loginActions.js
+++ frontend/src/Redux/loginActions.js
@@ -69,34 +69,3 @@ export function logout() {
     return fetch('/api/v1/logout');
   };
 }
-
-export const REQUEST_HOUSES = 'REQUEST_HOUSES';
-export function requestHouses(postCode) {
-  return {
-    type: REQUEST_HOUSES,
-    postCode
-  };
-}
-
-export const FETCH_HOUSES_SUCCESS = 'FETCH_HOUSES_SUCCESS';
-export function receiveHouses(postCode, json) {
-  return {
-    type: FETCH_HOUSES_SUCCESS,
-    postCode,
-    houses: json.data
-  };
-}
-
-export function fetchHouses(postCode) {
-  return function(dispatch) {
-    // Inform app that request is starting
-    dispatch(requestHouses(postCode));
-
-    return fetch(`/api/v1/houses?postCode=${postCode}`)
-      .then(
-        response => response.json(),
-        error => console.log('An error occurred.', error)
-      )
-      .then(json => dispatch(receiveHouses(postCode, json)));
-  };
-}
