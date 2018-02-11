7b63c3916a61e4ca5eb8233c40f521369884795f
nikrb <nik_rb@yahoo.com>
2018-02-11 01:19:24 +0000

    add login actions unit tests

---
+++ frontend/src/Redux/loginActions.unit.spec.js
@@ -0,0 +1,49 @@
+import {expect} from 'chai';
+import * as actions from './loginActions';
+import * as UserTypes from './UserTypes';
+
+describe( 'login actions', () => {
+  const user = {email:"e@g.com", name:"e"};
+  it( 'should create an action when login successful', () => {
+    const expectedAction = {
+      type: UserTypes.REQUEST_LOGIN_SUCCESS,
+      user
+    };
+    expect(actions.requestLoginSuccess(user)).to.eql(expectedAction);
+  });
+  it( 'should create an action when login fails', () => {
+    const error = "some error";
+    const expectedAction = {
+      type: UserTypes.REQUEST_LOGIN_FAILED,
+      error
+    };
+    expect(actions.requestLoginFailed(error)).to.eql(expectedAction);
+  });
+  it( 'should create an action when error cleared', () => {
+    const expectedAction = {
+      type: UserTypes.CLEAR_LOGIN_ERROR
+    };
+    expect(actions.clearLoginError()).to.eql(expectedAction);
+  });
+  it( 'should create an action when auto login successful', () => {
+    const expectedAction = {
+      type: UserTypes.AUTO_LOGIN_SUCCESS,
+      user
+    };
+    expect(actions.autoLoginSuccess(user)).to.eql(expectedAction);
+  });
+  it( 'should create an action when auto login fails', () => {
+    const error = "some error";
+    const expectedAction = {
+      type: UserTypes.AUTO_LOGIN_FAILED,
+      error
+    };
+    expect(actions.autoLoginFailed(error)).to.eql(expectedAction);
+  });
+  it( 'should create an action when logout requested', () => {
+    const expectedAction = {
+      type: UserTypes.LOGOUT
+    };
+    expect(actions.requestLogout()).to.eql(expectedAction);
+  });
+});
