738e6578c0f2ace73523bf858e90e372e25e4784
nikrb <nik_rb@yahoo.com>
2018-10-07 08:40:22 +0100

    fix: display default avatar pic

--- frontend/src/Image/ImageRef.js
+++ frontend/src/Image/ImageRef.js
@@ -4,7 +4,7 @@ export default function ImageRef(src, missing_url) {
   return new Promise( (resolve, reject) => {
     // check for undefined, url, image mongo id, file
     if ( typeof src === 'undefined' || src === null) {
-        reject(encodeURI( missing_url));
+      resolve(encodeURI( missing_url));
     } else if (typeof src === 'string') {
       if( src.indexOf('/') === -1) {
         loadImage( src)
